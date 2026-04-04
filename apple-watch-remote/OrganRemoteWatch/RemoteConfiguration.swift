fd3978e27658e93de1f28829d860689d6f5a4da0
John Roper <johnroper100@gmail.com>
2026-04-04 13:31:47 -0400

    fixes for gitignore

--- apple-watch-remote/OrganRemoteWatch/RemoteConfiguration.swift
+++ apple-watch-remote/OrganRemoteWatch/RemoteConfiguration.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum RemoteConfiguration {
     // Use the same URL you already open in Safari for the tuner UI.
-    static let serverInput = "http://localhost:3000/tuner"
+    static let serverInput = "http://192.168.68.108:3000/tuner"
 
     static let backCommand = "/OPTICS/special2014"
     static let nextCommand = "/OPTICS/special2015"
