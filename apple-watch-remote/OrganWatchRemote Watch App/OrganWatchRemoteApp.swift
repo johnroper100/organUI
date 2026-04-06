53849d66ac33a3db1e53d9403f7123be8eb044fc
John Roper <johnroper100@gmail.com>
2026-04-06 15:29:53 -0400

    update app

---
+++ apple-watch-remote/OrganWatchRemote Watch App/OrganWatchRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganWatchRemoteApp.swift
+//  OrganWatchRemote Watch App
+//
+//  Created by John Roper on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct OrganWatchRemote_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
